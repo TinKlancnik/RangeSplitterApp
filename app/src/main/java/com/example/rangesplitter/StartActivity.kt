e413771b543bfc69fd5e8f3c8af7c08cc195405b
TinKlancnik <tin.klancnik@student.um.si>
2024-09-05 20:05:37 +0200

    login activity design changed

--- app/src/main/java/com/example/rangesplitter/StartActivity.kt
+++ app/src/main/java/com/example/rangesplitter/StartActivity.kt
@@ -1,16 +1,23 @@
 package com.example.rangesplitter
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity: AppCompatActivity (){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        // Hide the action bar
-        supportActionBar?.hide()
 
-        // Make the navigation bar black
+        supportActionBar?.hide()
         window.navigationBarColor = resources.getColor(android.R.color.black)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
